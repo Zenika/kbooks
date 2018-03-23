b8820c55aa94ba10f4a07843f41f8efff0855b31
Vincent Poilvert <v.poilvert@gmail.com>
2018-03-23 09:00:32 +0100

    Add transactional annotation

--- src/main/kotlin/com/zenika/kbooks/feature/book/BookService.kt
+++ src/main/kotlin/com/zenika/kbooks/feature/book/BookService.kt
@@ -4,6 +4,7 @@ import com.zenika.kbooks.util.rest.PageDto
 import com.zenika.kbooks.util.rest.PaginationDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import javax.ws.rs.*
 
 /**
@@ -14,17 +15,20 @@ class BookService {
     @Autowired
     private lateinit var bookRepository: IBookRepository
 
+    @Transactional(readOnly = true)
     fun getBook(@PathParam("id") id: Long): BookDto {
         return bookRepository.findById(id)
                 .map { BookDtoConverter().convert(it) }
                 .orElseThrow(::NotFoundException)
     }
 
+    @Transactional(readOnly = true)
     fun getBooks(pagination: PaginationDto): PageDto<BookDto> {
         var page = bookRepository.findAll(pagination.toPageable())
         return BookDtoConverter().convert(page)
     }
 
+    @Transactional
     fun createBook(dto: BookDto): Long? {
         val book = Book()
         book.title = dto.title
@@ -34,6 +38,7 @@ class BookService {
         return bookRepository.save(book).id
     }
 
+    @Transactional
     fun updateBook(@PathParam("id") id: Long, dto: BookDto) {
         val book = bookRepository.findById(id).orElseThrow(::NotFoundException)
         book.title = dto.title
@@ -42,6 +47,7 @@ class BookService {
         bookRepository.save(book)
     }
 
+    @Transactional
     fun deleteBook(@PathParam("id") id: Long) {
         val book = bookRepository.findById(id).orElseThrow(::NotFoundException)
         bookRepository.delete(book)
